9ec418377810c83e10a979f90f64dc0a63cb7192
Bhuvan Shah <bhuvanshah288@gmail.com>
2024-10-30 16:20:06 -0700

    changed loc of config

--- document-service/src/config.js
+++ document-service/src/config.js
@@ -4,5 +4,4 @@ const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000'
 };
-
-export default config;
+module.exports = config;
