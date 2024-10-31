7109e3be9aa0f2a5be3492c8018af7c861c8be2c
Bhuvan Shah <bhuvanshah288@gmail.com>
2024-10-30 23:31:13 -0700

    Update config.js

--- document-service/src/config.js
+++ document-service/src/config.js
@@ -2,6 +2,8 @@ const config = {
     AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || 'http://localhost:3001',
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    DOCUMENT_URL: process.env.REACT_APP_DOCUMENT_URL || 'http://localhost:3002',
+    SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://localhost:3003',
     FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000'
 };
 module.exports = config;
